8143ec88e49526b501ee344c6e9f3575afecd976
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2020-11-02 11:12:25 -0500

    Add a title field to a user (#5)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
+            $table->string('title')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
