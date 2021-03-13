5e25f5a9697dc0beb634a5c1a0337b2808a3ad9c
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2021-03-13 11:51:08 -0500

    Apply reordering of imports

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\TeachersController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
