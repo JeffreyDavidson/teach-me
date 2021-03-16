7fa1576c95cb5fc985556a5a6d052da83e0ea69f
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2021-03-15 20:24:19 -0400

    Add authorization check for creating a teacher

--- app/Http/Requests/CreateTeacherRequest.php
+++ app/Http/Requests/CreateTeacherRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Teacher;
 use Illuminate\Foundation\Http\FormRequest;
 
 class CreateTeacherRequest extends FormRequest
@@ -13,7 +14,7 @@ class CreateTeacherRequest extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return auth()->user()->can('create', Teacher::class);
     }
 
     /**
