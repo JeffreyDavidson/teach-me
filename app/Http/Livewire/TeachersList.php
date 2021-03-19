738289f6cdcc6414c2c9996d519c7d9c89bccb6e
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2021-03-18 20:55:20 -0400

    Adding traits for datatable

--- app/Http/Livewire/TeachersList.php
+++ app/Http/Livewire/TeachersList.php
@@ -2,27 +2,42 @@
 
 namespace App\Http\Livewire;
 
+use App\Http\Livewire\DataTable\WithPerPagePagination;
+use App\Http\Livewire\DataTable\WithSorting;
 use App\Models\Teacher;
 use Livewire\Component;
-use Livewire\WithPagination;
 
 class TeachersList extends Component
 {
-    use WithPagination;
+    use WithPerPagePagination, WithSorting;
 
     protected $paginationTheme = 'bootstrap';
 
-    public $perPage = 10;
+    protected $queryString = ['sorts'];
 
     public function updatedPerPage($value)
     {
         $this->resetPage();
     }
 
+    public function getRowsQueryProperty()
+    {
+        $query = Teacher::query()
+            ->select('*')
+            ->selectRaw("CONCAT(last_name, ', ', first_name) as full_name");
+
+        return $this->applySorting($query);
+    }
+
+    public function getRowsProperty()
+    {
+        return $this->applyPagination($this->rowsQuery);
+    }
+
     public function render()
     {
         return view('livewire.teachers.teachers-list', [
-            'teachers' => Teacher::paginate($this->perPage),
+            'teachers' => $this->rows,
         ]);
     }
 }
