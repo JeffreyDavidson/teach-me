52d1bd65183b0d07e1d65e8bb9f5b765add464bd
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2021-03-24 13:47:14 -0400

    Fix failing test

--- tests/Integration/Views/TeacherViewsTest.php
+++ tests/Integration/Views/TeacherViewsTest.php
@@ -1,19 +1,19 @@
 <?php
 
-namespace Tests\Integration;
+namespace Tests\Integration\Views;
 
 use App\Models\Administrator;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ViewsTest extends TestCase
+class TeacherViewsTest extends TestCase
 {
     use RefreshDatabase;
 
     /** @test */
     public function teachers_index_uses_teachers_list_livewire_component()
     {
-        $response = $this->actingAs(Administrator::factory()->create())->get('teachers.index');
+        $response = $this->actingAs(Administrator::factory()->create())->get(route('teachers.index'));
 
         $response->assertSeeLivewire('teachers-list');
     }
