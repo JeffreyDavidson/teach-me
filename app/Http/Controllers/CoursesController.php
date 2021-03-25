58536f43400db65002487a2169e744f8ee88610a
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2021-03-25 09:13:58 -0400

    Add updating of a course

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateCourseRequest;
+use App\Http\Requests\UpdateCourseRequest;
 use App\Models\Course;
 use App\Services\CourseService;
 
@@ -43,6 +44,34 @@ class CoursesController extends Controller
     public function store(CreateCourseRequest $request, CourseService $courseService)
     {
         $courseService->create($request->validated());
+
+        return redirect()->route('courses.index');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  App\Models\Course
+     * @return \Illuminate\View\View
+     */
+    public function edit(Course $course)
+    {
+        $this->authorize('update', $course);
+
+        return view('courses.edit', compact('course'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  App\Http\Requests\UpdateCourseRequest $request
+     * @param  App\Models\Course $course
+     * @param  App\Services\CourseService $courseService
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(UpdateCourseRequest $request, Course $course, CourseService $courseService)
+    {
+        $courseService->update($course, $request->validated());
 
         return redirect()->route('courses.index');
     }
---
+++ resources/views/courses/edit.blade.php
@@ -0,0 +1,40 @@
+<x-layout>
+    <x-slot name="subheader">
+        <x-subheader title="teachers">
+            <x-slot name="breadcrumb">
+                <x-breadcrumb>
+                    <li class="breadcrumb-item"><a class="text-muted" href="#">Academics</a></li>
+                    <li class="breadcrumb-item"><a class="text-muted" href="#">Courses</a></li>
+                    <li class="breadcrumb-item"><a class="text-muted" href="#">Edit {{ $course->name }}</a></li>
+                </x-breadcrumb>
+            </x-slot>
+        </x-subheader>
+    </x-slot>
+
+    <!--begin::Card-->
+    <div class="card card-custom">
+        <!--begin::Header-->
+        <div class="card-header">
+            <div class="card-title">
+                <h3 class="card-label">Edit Course Form
+            </div>
+        </div>
+        <!--end::Header-->
+        <form method="post" action="{{ route('courses.update', $course) }}">
+            @csrf
+            @method('PATCH')
+            <!--begin::Body-->
+            <div class="card-body">
+                @include('courses.partials.form')
+            </div>
+            <!--end::Body-->
+            <!--begin::Footer-->
+            <div class="card-footer">
+                <button type="submit" class="btn btn-primary font-weight-bold">Submit</button>
+                <button type="button" class="btn btn-light-primary font-weight-bold" data-dismiss="modal">Close</button>
+            </div>
+            <!--end::Footer-->
+        </form>
+    </div>
+    <!--end::Card-->
+</x-layout>
