3f1d257387823c16de78a73c7fb5d227cf08709e
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2021-03-25 08:12:44 -0400

    Feature/view courses list (#21)
    * Add viewing of courses list
    
    * Add seeding of courses
    
    * Add course livewire test

---
+++ app/Http/Livewire/CoursesList.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Http\Livewire\DataTable\WithPerPagePagination;
+use App\Http\Livewire\DataTable\WithSorting;
+use App\Models\Course;
+use Livewire\Component;
+
+class CoursesList extends Component
+{
+    use WithPerPagePagination, WithSorting;
+
+    protected $paginationTheme = 'bootstrap';
+
+    public $showFilters = false;
+    public $filters = [
+        'search' => '',
+    ];
+
+    protected $queryString = ['sorts'];
+
+    public function updatedPerPage($value)
+    {
+        $this->resetPage();
+    }
+
+    public function toggleShowFilters()
+    {
+        $this->showFilters = ! $this->showFilters;
+    }
+
+    public function resetFilters()
+    {
+        $this->reset('filters');
+    }
+
+    public function getRowsQueryProperty()
+    {
+        $query = Course::query()
+            ->when($this->filters['search'], function ($query, $search) {
+                $query->where('name', 'like', '%'.$search.'%');
+            });
+
+        return $this->applySorting($query);
+    }
+
+    public function getRowsProperty()
+    {
+        return $this->applyPagination($this->rowsQuery);
+    }
+
+    public function render()
+    {
+        return view('livewire.courses.courses-list', [
+            'courses' => $this->rows,
+        ]);
+    }
+}
---
+++ resources/views/courses/index.blade.php
@@ -0,0 +1,34 @@
+<x-layout>
+    <x-slot name="subheader">
+        <x-subheader title="Courses">
+            <x-slot name="breadcrumb">
+                <x-breadcrumb>
+                    <li class="breadcrumb-item"><a class="text-muted" href="#">Academics</a></li>
+                    <li class="breadcrumb-item"><a class="text-muted" href="#">Courses</a></li>
+                </x-breadcrumb>
+            </x-slot>
+        </x-subheader>
+    </x-slot>
+
+    <!--begin::Card-->
+    <div class="card card-custom">
+        <!--begin::Header-->
+        <div class="card-header">
+            <div class="card-title">
+                <h3 class="card-label">Course List
+            </div>
+            <div class="card-toolbar">
+                <a class="btn btn-primary font-weight-bolder" href="{{ route('courses.create') }}">New Record</a>
+            </div>
+        </div>
+        <!--end::Header-->
+        <!--begin::Body-->
+        <div class="card-body">
+            <!--begin: Datatable-->
+            <livewire:courses-list />
+            <!--end: Datatable-->
+        </div>
+        <!--end::Body-->
+    </div>
+    <!--end::Card-->
+</x-layout>
