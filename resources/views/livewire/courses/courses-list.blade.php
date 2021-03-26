a907b48d30cc601797b8e92ed3c7d86730b05d52
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2021-03-26 11:30:09 -0400

    Create a pagination component for resources datatables (#27)

--- resources/views/livewire/courses/courses-list.blade.php
+++ resources/views/livewire/courses/courses-list.blade.php
@@ -40,18 +40,6 @@
             </x-slot>
         </x-table>
 
-        <div class="datatable-pager datatable-paging-loaded">
-            {{ $courses->links() }}
-            <div class="datatable-pager-info my-2 mb-sm-0">
-                <select wire:model="perPage" class="datatable-pager-size">
-                    <option value="5">5</option>
-                    <option value="10">10</option>
-                    <option value="20">20</option>
-                    <option value="30">30</option>
-                    <option value="50">50</option>
-                </select>
-                <span class="datatable-pager-detail">Showing {{ $courses->firstItem() }} - {{ $courses->lastItem() }} of {{ $courses->total() }}</span>
-            </div>
-        </div>
+        <x-pagination :collection="$courses" />
     </div>
 </div>
