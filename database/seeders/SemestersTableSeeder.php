9174b69ab0918b0da019a8910546404e1aa6c68d
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2021-03-30 14:05:53 -0400

    Add course sections for a semester

--- database/seeders/SemestersTableSeeder.php
+++ database/seeders/SemestersTableSeeder.php
@@ -29,7 +29,23 @@ class SemestersTableSeeder extends Seeder
         }
 
         foreach ($semesters as $semester) {
-            Semester::factory()->create(['name' => $semester]);
+            if (Str::of($semester)->startsWith('Spring')) {
+                $startDate = Carbon::parse('first day of February '.Str::of($semester)->after('Spring '));
+                $endDate = Carbon::parse('first day of June '.Str::of($semester)->after('Spring '));
+            } elseif (Str::of($semester)->startsWith('Summer')) {
+                $startDate = Carbon::parse('first day of June '.Str::of($semester)->after('Summer '));
+                $endDate = Carbon::parse('first day of September '.Str::of($semester)->after('Summer '));
+            } elseif (Str::of($semester)->startsWith('Fall')) {
+                $startDate = Carbon::parse('first day of September '.Str::of($semester)->after('Fall '));
+                $yearString = (string) Str::of($semester)->after('Fall ');
+                $yearInt = (int) $yearString + 1;
+                $endDate = Carbon::parse('first day of January '.(string) $yearInt);
+            }
+            Semester::factory()
+                ->has(CourseSection::factory()->count(20)->state(new Sequence(
+                    fn () => ['course_id' => Course::inRandomOrder()->first(), 'start_date' => $startDate, 'end_date' => $endDate],
+                )))
+                ->create(['name' => $semester]);
         }
     }
 }
