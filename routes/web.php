e2cd0d961b26302e85cbcb97e53d862f9b497e1a
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2021-03-30 14:12:59 -0400

    Removing code not for this feature

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\SemestersController;
 use App\Http\Controllers\StudentsController;
 use App\Http\Controllers\TeachersController;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,8 +18,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-// Auth::loginUsingId(1);
 
 Route::get('/', function () {
     return view('welcome');
