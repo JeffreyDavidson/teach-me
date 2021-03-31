91515847ec1712e111f454fe833d89f076c0257f
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2021-03-31 14:16:59 -0400

    Add new route key to semesters

--- app/Models/Semester.php
+++ app/Models/Semester.php
@@ -4,10 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Semester extends Model
 {
-    use HasFactory;
+    use HasFactory, HasSlug;
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
 
     /**
      * Get course sections for the semester.
