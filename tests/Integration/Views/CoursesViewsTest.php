4dd6960343ecdc74d42ae64dcf6a2d773ef04810
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2021-03-31 13:17:38 -0400

    Add integration testing for course details page

--- tests/Integration/Views/CoursesViewsTest.php
+++ tests/Integration/Views/CoursesViewsTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Integration\Views;
 
 use App\Models\Administrator;
+use App\Models\Course;
+use App\Models\CourseSection;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -17,5 +19,16 @@ class CoursesViewsTest extends TestCase
             ->actingAs(Administrator::factory()->create())
             ->get(route('courses.index'))
             ->assertSeeLivewire('courses-list');
+    }
+
+    /** @test */
+    public function course_show_uses_course_sections_list_livewire_component()
+    {
+        $course = Course::factory()->has(CourseSection::factory()->count(3), 'sections')->create();
+
+        $this
+            ->actingAs(Administrator::factory()->create())
+            ->get(route('courses.show', $course))
+            ->assertSeeLivewire('course-sections-list');
     }
 }
