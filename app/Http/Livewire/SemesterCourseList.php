f690b120a93141b370dd49b1a6907cbdd8610fe7
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2021-04-01 14:00:58 -0400

    Add docblocks for component

--- app/Http/Livewire/SemesterCourseList.php
+++ app/Http/Livewire/SemesterCourseList.php
@@ -13,37 +13,90 @@ class SemesterCourseList extends Component
 {
     use WithPerPagePagination, WithSorting;
 
+    /**
+     * Pagination theme to be used for view.
+     *
+     * @var string
+     */
     protected $paginationTheme = 'bootstrap';
 
+    /**
+     * Accepted keys for the query string.
+     *
+     * @var array
+     */
+    protected $queryString = ['sorts'];
+
+    /**
+     * Define on if filters should be shown by default.
+     *
+     * @var bool
+     */
     public $showFilters = false;
+
+    /**
+     * Define allowed filters for page.
+     *
+     * @var array
+     */
     public $filters = [
         'search' => '',
     ];
 
-    protected $queryString = ['sorts'];
-
+    /**
+     * Semester to be used for gathering courses.
+     *
+     * @var App\Models\Semester
+     */
     public Semester $semester;
 
+    /**
+     * Apply properties to the instance.
+     *
+     * @param  App\Models\Semester $semester
+     * @return void
+     */
     public function mount(Semester $semester)
     {
         $this->semester = $semester;
     }
 
+    /**
+     * Undocumented function.
+     *
+     * @param  int $value
+     * @return void
+     */
     public function updatedPerPage($value)
     {
         $this->resetPage();
     }
 
+    /**
+     * Toggle the visibility of page filters.
+     *
+     * @return void
+     */
     public function toggleShowFilters()
     {
         $this->showFilters = ! $this->showFilters;
     }
 
+    /**
+     * Reset all filters to default values.
+     *
+     * @return void
+     */
     public function resetFilters()
     {
         $this->reset('filters');
     }
 
+    /**
+     * Retrieve sorted courses attached to the semester.
+     *
+     * @return void
+     */
     public function getRowsQueryProperty()
     {
         $query = Course::query()
@@ -58,11 +111,21 @@ class SemesterCourseList extends Component
         return $this->applySorting($query);
     }
 
+    /**
+     * Paginate component data collection.
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
     public function getRowsProperty()
     {
         return $this->applyPagination($this->rowsQuery);
     }
 
+    /**
+     * Show the courses for the semester.
+     *
+     * @return \Illuminate\View\View
+     */
     public function render()
     {
         return view('livewire.semesters.course-list', [
