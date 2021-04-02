387c955a75979807500d46ab731d18022322e0be
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2021-04-02 10:18:45 -0400

    Refactor for readability

--- database/seeders/SemestersTableSeeder.php
+++ database/seeders/SemestersTableSeeder.php
@@ -8,6 +8,7 @@ use App\Models\CourseSemester;
 use App\Models\Semester;
 use App\Models\Teacher;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Seeder;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Str;
@@ -25,47 +26,132 @@ class SemestersTableSeeder extends Seeder
     {
         $this->setUpFaker();
 
-        $seasons = ['Spring', 'Summer', 'Fall'];
-        $years = [date('Y'), date('Y', strtotime('-1 year')), date('Y', strtotime('-2 years'))];
-        $semesters = [];
-        foreach ($years as $year) {
-            foreach ($seasons as $season) {
-                $semesters[] = "{$season} {$year}";
-            }
-        }
+        $this->generateSemesters();
+    }
 
-        foreach ($semesters as $semester) {
-            if (Str::of($semester)->startsWith('Spring')) {
-                $startDate = Carbon::parse('first day of February '.Str::of($semester)->after('Spring '));
-                $endDate = Carbon::parse('first day of June '.Str::of($semester)->after('Spring '));
-            } elseif (Str::of($semester)->startsWith('Summer')) {
-                $startDate = Carbon::parse('first day of June '.Str::of($semester)->after('Summer '));
-                $endDate = Carbon::parse('first day of September '.Str::of($semester)->after('Summer '));
-            } elseif (Str::of($semester)->startsWith('Fall')) {
-                $startDate = Carbon::parse('first day of September '.Str::of($semester)->after('Fall '));
-                $yearString = (string) Str::of($semester)->after('Fall ');
-                $yearInt = (int) $yearString + 1;
-                $endDate = Carbon::parse('first day of January '.(string) $yearInt);
-            }
-
-            $semesterModel = Semester::factory()->create(['name' => $semester]);
+    /**
+     * Generate semesters needed for seeder.
+     *
+     * @return void
+     */
+    protected function generateSemesters()
+    {
+        foreach ($this->generateSemesterNames() as $semesterName) {
+            $semester = Semester::factory()->create(['name' => $semesterName]);
             $courses = Course::inRandomOrder()->take(5)->get();
 
-            foreach ($courses as $course) {
-                $courseSemester = CourseSemester::factory()->create([
-                    'semester_id' => $semesterModel,
-                    'course_id' => $course->id,
-                    'start_date' => $startDate,
-                    'end_date' => $endDate,
-                ]);
+            $this->createCourseRequirements($semester, $courses);
+        }
+    }
 
-                CourseSection::factory()->create([
-                    'course_semester_id' => $courseSemester->id,
-                    'teacher_id' => Teacher::inRandomOrder()->first()->id,
-                    'start_time' => $startTime = Carbon::parse($this->faker->time('H:00')),
-                    'end_time' => $startTime->copy()->addHour(),
-                ]);
+    /**
+     * Create semester names for last two years.
+     *
+     * @return array
+     */
+    protected function generateSemesterNames()
+    {
+        $seasons = ['Spring', 'Summer', 'Fall'];
+        $years = [date('Y'), date('Y', strtotime('-1 year')), date('Y', strtotime('-2 years'))];
+        $semesterNames = [];
+
+        foreach ($years as $year) {
+            foreach ($seasons as $season) {
+                $semesterNames[] = "{$season} {$year}";
             }
         }
+
+        return $semesterNames;
+    }
+
+    /**
+     * Creates dates for each course during the semester.
+     *
+     * @param  string $semesterName
+     * @return array
+     */
+    protected function generateCourseDates($semesterName)
+    {
+        $dates = [];
+
+        if (Str::of($semesterName)->startsWith('Spring')) {
+            $dates['startDate'] = Carbon::parse('first day of February '.Str::of($semesterName)->after('Spring '));
+            $dates['endDate'] = Carbon::parse('first day of June '.Str::of($semesterName)->after('Spring '));
+        } elseif (Str::of($semesterName)->startsWith('Summer')) {
+            $dates['startDate'] = Carbon::parse('first day of June '.Str::of($semesterName)->after('Summer '));
+            $dates['endDate'] = Carbon::parse('first day of September '.Str::of($semesterName)->after('Summer '));
+        } elseif (Str::of($semesterName)->startsWith('Fall')) {
+            $dates['startDate'] = Carbon::parse('first day of September '.Str::of($semesterName)->after('Fall '));
+            $yearString = (string) Str::of($semesterName)->after('Fall ');
+            $yearInt = (int) $yearString + 1;
+            $dates['endDate'] = Carbon::parse('first day of January '.(string) $yearInt);
+        }
+
+        return $dates;
+    }
+
+    /**
+     * Create course requirements during a semester.
+     *
+     * @param  \App\Models\Semester $semester
+     * @param  \App\Models\Course $course
+     * @return void
+     */
+    protected function createCourseRequirement($semester, $course)
+    {
+        $courseDates = $this->generateCourseDates($semester->name);
+
+        $courseSemester = $this->createCourseSemester($semester, $course, $courseDates['startDate'], $courseDates['endDate']);
+
+        $this->createCourseSection($courseSemester);
+    }
+
+    /**
+     * Create coure requirements for each course in a semester.
+     *
+     * @param  \App\Models\Semester $semester
+     * @param  \Illuminate\Database\Eloquent\Collection $courses
+     * @return void
+     */
+    protected function createCourseRequirements(Semester $semester, Collection $courses)
+    {
+        foreach ($courses as $course) {
+            $this->createCourseRequirement($semester, $course);
+        }
+    }
+
+    /**
+     * Create a course section for a course in a semester.
+     *
+     * @param  \App\Models\CourseSemester $courseSemester
+     * @return void
+     */
+    protected function createCourseSection(CourseSemester $courseSemester)
+    {
+        CourseSection::factory()->create([
+            'course_semester_id' => $courseSemester->id,
+            'teacher_id' => Teacher::inRandomOrder()->first()->id,
+            'start_time' => $startTime = Carbon::parse($this->faker->time('H:00')),
+            'end_time' => $startTime->copy()->addHour(),
+        ]);
+    }
+
+    /**
+     * Create a course semester.
+     *
+     * @param  \App\Models\Semester $semester
+     * @param  \App\Models\Course $course
+     * @param  string $startDate
+     * @param  string $endDate
+     * @return \App\Models\CourseSemester
+     */
+    protected function createCourseSemester($semester, $course, $startDate, $endDate)
+    {
+        return CourseSemester::factory()->create([
+            'semester_id' => $semester->id,
+            'course_id' => $course->id,
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+        ]);
     }
 }
