a4ec2ee093148d9dd5194eed64c8d8952f94a99d
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2021-04-07 09:40:33 -0400

    Refactor views to search component (#41)
    * Create search blade component
    
    * Apply search blade component to Livewire blade files

--- resources/views/livewire/courses/courses-list.blade.php
+++ resources/views/livewire/courses/courses-list.blade.php
@@ -1,12 +1,7 @@
 <div>
     <div class="mb-7">
         <div class="row align-items-center">
-            <div class="col-md-6 my-2 my-md-0">
-                <div class="input-icon mr-4">
-                    <input type="text" wire:model="filters.search" class="form-control" id="search_query" placeholder="Search...">
-                    <span><i class="flaticon2-search-1 text-muted"></i></span>
-                </div>
-            </div>
+            <x-search />
         </div>
     </div>
 
