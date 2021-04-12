059ed5ec360e475b65253d7100bca23f08f57d57
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2021-04-12 11:13:42 -0400

    Update select component to handle multiple selected options

--- resources/views/components/input/select.blade.php
+++ resources/views/components/input/select.blade.php
@@ -1,10 +1,11 @@
 @props([
     'options' => [],
-    'selectedOption' => ''
+    'selectedOptions' => [],
+    'multiple' => false
 ])
 
-<select {{ $attributes->merge(['class' => 'form-control']) }}>
+<select {{ $attributes->merge(['class' => 'form-control']) }} @if ($multiple) multiple @endif>
     @foreach($options as $value => $label)
-        <option value="{{ $value }}" {{ $selectedOption == $value ? 'selected="selected"' : ''}}>{{ $label }}</option>
+        <option value="{{ $value }}" {{ in_array($value, array_keys($selectedOptions)) ? 'selected=selected' : ''}}>{{ $label }}</option>
     @endforeach
 </select>
